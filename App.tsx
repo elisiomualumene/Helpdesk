78b132cc7fe53fa6fcb698a63ee7412164a86436
Elisio Mualumene <elisio.mualumene@nzooji.com>
2022-09-16 22:45:02 +0100

    :zap: fake data and new dependecies

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // 1. import `NativeBaseProvider` component
 import { NativeBaseProvider } from 'native-base';
 import { Routes } from './src/routes';
+
 export default function App() {
   // 2. Use at the root of your app
   return (
---
+++ src/mock/db.ts
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        id: 1,
+        machine: "10223",
+        description: "Problemas em ligar",
+        createAt: "16/09/2022"
+    },
+    {
+        id: 2,
+        machine: "32992",
+        description: "Problemas em ligar",
+        createAt: "15/09/2022"
+    },
+    {
+        id: 3,
+        machine: "15223",
+        description: "Problemas em ligar",
+        createAt: "14/09/2022"
+    },
+    {
+        id: 4,
+        machine: "16223",
+        description: "Problemas em ligar",
+        createAt: "12/09/2022"
+    },
+    {
+        id: 5,
+        machine: "13523",
+        description: "Problemas em ligar",
+        createAt: "10/09/2022"
+    },
+    {
+        id: 6,
+        machine: "10567",
+        description: "Problemas em ligar",
+        createAt: "4/09/2022"
+    },
+    {
+        id: 7,
+        machine: "10567",
+        description: "Problemas em ligar",
+        createAt: "1/09/2022"
+    },
+]
